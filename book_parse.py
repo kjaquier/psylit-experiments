298db14051f3066b0e1f31001315d167da037c83
Kevin Jaquier <kevin.jaquier@gmail.com>
2019-07-01 19:42:08 +1200

    WIP resolving person/environment

--- book_parse.py
+++ book_parse.py
@@ -1,12 +1,3 @@
-#%% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataSciece.changeDirOnImportExport setting
-import os
-try:
-	os.chdir(r'C:\Users\kevin\Documents\Workspace\psylit-experiments')
-	print(os.getcwd())
-except Exception as e:
-	print(e)
-#%%
-
 import pandas as pd
 import numpy as np
 
@@ -21,15 +12,6 @@ import processing.preprocess as preprocess
 import spacy
 from spacy_wordnet.wordnet_annotator import WordnetAnnotator 
 import neuralcoref
-
-#%%
-
-data_root = r'..\datasets\2_txtalb_Novel450'
-book_filename = r'\EN_1818_Shelley,Mary_Frankenstein_Novel.txt'
-
-
-
-#%%
 
 def build_pipe(model='en_core_web_sm'):
 
@@ -56,14 +38,33 @@ def build_pipe(model='en_core_web_sm'):
 	predparser = myspacy.PredicateParser(nlp.vocab)
 	nlp.add_pipe(predparser)
 
+	hypernym_map = {'person.n.01': 'person', 
+                     'female.n.02':'female', 
+                     'woman.n.01':'female',
+                     'man.n.01':'male',
+                     'male.n.02': 'male', 
+                     'entity.n.01': 'entity'}
+
+	# hypermatch = myspacy.HypernymMatcher(nlp.vocab, 
+	# 									 hypernym_map,
+	# 								 	 pattern=[{'_': {'in_coref': True}}],
+	# 								     highest_level=2, 
+	# 									 highest_common_level=2,
+	# 									 attr_name='wn_peopletype')
+	#nlp.add_pipe(hypermatch)
+
+	mention_matcher = myspacy.tok_hypernyms_matcher(hypernym_map.keys(),
+													highest_level=2,
+													highest_common_level=2)
+
+	mention_tagger = lambda mention: \
+		{hypernym_map[m] for m in mention_matcher(mention.root)}
+
+	ent_tag = myspacy.EntityTagger(mention_tagger)
+	nlp.add_pipe(ent_tag)
+
 	return nlp
 
-#
-
-#%%
-#
-
-#%%
 
 def get_dataframe(doc):
 	fmt = lambda tok: tok.text.strip().lower()[:50] if tok else None
@@ -74,7 +75,7 @@ def get_dataframe(doc):
 		'lemma': tok.lemma_[:50],
 		'text': fmt(tok),
 		**{('R_'+r): fmt(t) for r, t in tok._.sem_deps},
-		**{('L_'+doc.vocab[cat].text): 1.0 for cat in tok._.lex},
+		**{('L_'+doc.vocab[cat].text): 1.0 for cat in tok._.lex}, 
 		}
 		for tok in doc if tok._.has_lex
 	]
@@ -86,13 +87,84 @@ def get_dataframe(doc):
 	return table
 
 
-#%%
+def generate_ent_type(ents, level='entity'):
+    n = len(ents.index)
+    
+    pos = ents['entity_pos' if level == 'entity' else 'mention_pos']
+    
+    is_relevant = pos.isin(['PROPN','NOUN','DET','PRON'])
+    resolved = pos == 'PROPN'
+    is_noun = pos == 'NOUN'
+    is_irrelevant = ~is_relevant | (ents.NER_CARDINAL > 0) | (ents.NER_DATE > 0)
+    
+    is_noun_for_person = is_noun & (ents.WN_person > 0)
+    is_person = resolved | is_noun_for_person
+    is_unknown = ents.entity_pos.isin(['DET','PRON'])
+
+    ent_type = pd.Series(np.array(['person'] * n))
+    ent_type[~is_person] = 'environment'
+    ent_type[is_unknown] = 'unknown'
+    ent_type[is_irrelevant] = None
+    ent_type = pd.Categorical(ent_type)
+    return ent_type
+
+
+def get_entities_df(doc):
+        
+    ent_type_hypernym_map = {'person.n.01': 'person', 
+                             'female.n.02':'female', 
+                             'woman.n.01':'female',
+                             'man.n.01':'male',
+                             'male.n.02': 'male', 
+                             'entity.n.01': 'entity'}
+        
+    ent_type_matcher = myspacy.tok_hypernyms_matcher(ent_type_hypernym_map.keys(),
+                                                     highest_level=2,
+                                                     highest_common_level=2)
+    
+    def process(span):
+        x = span.root
+        pos = x.pos_
+        return x.text.strip() if pos == 'PROPN' else pos
+
+    ent_rows = []
+    for clust in doc._.coref_clusters:
+        e = clust.main
+        e_root = e.root.text
+        e_pos = e.root.pos_
+        e_processed = process(e)
+        e_txt = e.text
+        for mention in clust.mentions:
+            ner_tags = {f'NER_{t.ent_type_}':1 for t in mention if t.ent_type_}
+            wn_tags = {f'WN_{ent_type_hypernym_map[m.name()]}':1 for m in ent_type_matcher(mention.root)}
+            m_root = mention.root
+			ent_rows.append({
+                'i': mention.start,
+                't': m_root.sent.start,
+                'entity': e_processed,
+                'entity_root': e_root,
+                'entity_pos': e_pos,
+                'mention': process(mention),
+                'entity_text': e_txt,
+                'mention_text': mention.text,
+                'mention_pos': m_root.pos_,
+                **wn_tags,
+                **ner_tags
+            })
+    df = pd.DataFrame(ent_rows)
+    ner_cols = df.columns[list(df.columns.str.startswith('NER_'))]
+    df[ner_cols] = df[ner_cols].fillna(0)
+    wn_cols = df.columns[list(df.columns.str.startswith('WN_'))]
+    df[wn_cols] = df[wn_cols].fillna(0)
+    return df
+
 
 def main(input_filename:"Raw text of book to read (UTF-8)",
 		 output_filename:"CSV file to write",
 		 save_doc:("File where spacy Doc object is saved",'option','d')=None,
-		 start:("Position to read from",'option','s')=None,
-		 end:("Position to stop reading after",'option','e')=None):
+		 save_entities:("File where entities are saved", 'option','e')=None,
+		 start:("Position to read from",'option','t0')=None,
+		 end:("Position to stop reading after",'option','t1')=None):
 
 	txt = preprocess.read_pg(input_filename)
 	print('start=', repr(start), ' end=', repr(end))
@@ -107,6 +179,11 @@ def main(input_filename:"Raw text of book to read (UTF-8)",
 	nlp = build_pipe()
 	print("Pipeline: ", ', '.join(pname for pname, _ in nlp.pipeline))
 	doc = nlp(txt)
+
+	entities_df = get_entities_df(doc)
+
+	if save_entities:
+		entities_df.to_csv(save_entities)
 
 	if save_doc:
 		doc.to_disk(save_doc)
