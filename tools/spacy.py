648e5fc13996080b859ad802afc150f000582b9d
Kevin Jaquier <kevin.jaquier@gmail.com>
2019-07-01 19:42:08 +1200

    WIP resolving person/environment

--- tools/spacy.py
+++ tools/spacy.py
@@ -1,4 +1,7 @@
-from collections import Counter, namedtuple
+from itertools import combinations
+from collections import Counter, namedtuple, defaultdict
+
+from nltk.corpus import wordnet as wn
 from spacy.matcher import Matcher
 from spacy.tokens import Doc, Span, Token
 
@@ -129,6 +132,10 @@ class CorefParser__old:
     #    for r in self.relations:
     #        Token.remove_extension(r)
 
+AGENT_DEPS = {'nsubj', 'csubj', 'nsubjpass', 'poss'}
+PREDICATIVE_LEMMAS = ('be',)
+POSSESSIVE_LEMMAS = ('have', 'possess')
+
 class SemanticDepParser:
 
     name = 'semantic_dep'
@@ -143,12 +150,12 @@ class SemanticDepParser:
             main = clust.main
             for mention in clust:
                 root = mention.root
-                rel = 'agent' if root.dep_ in ('nsubj', 'csubj', 'nsubjpass') else 'patient'
+                rel = 'agent' if root.dep_ in AGENT_DEPS else 'patient'
                 for anc in root.ancestors:
                     anc_lemma = anc.lemma_
-                    if anc_lemma == 'be':
+                    if anc_lemma == PREDICATIVE_LEMMAS:
                         rel = 'predicative'
-                    elif anc_lemma in ('have', 'possess'):
+                    elif anc_lemma in POSSESSIVE_LEMMAS:
                         rel = 'possessive'
                     
                     anc._.sem_deps.append((rel, main))
@@ -177,3 +184,134 @@ class PredicateParser:
         return doc
             
 
+class HypernymsExtractor:
+    name = 'hypernyms'
+    
+    def __init__(self, vocab, pattern=None, highest_level=1, force_ext=True):
+        Token.set_extension('hypernyms', default=set(), force=force_ext)
+        if pattern:
+            self.matcher = Matcher(vocab)
+            self.matcher.add('hypernyms', None, pattern)
+        else:
+            self.matcher = None
+        self.highest_level = highest_level
+    
+    def __call__(self, doc):
+        if self.matcher:
+            matches = self.matcher(doc)
+            print(f"[{self.__class__.__name__}] {len(matches)} matches")
+            toks = (doc[start:end].root for _, start, end in matches)
+        else:
+            toks= iter(doc)
+        
+        for t in toks:
+            
+            hypers = {h for s in t._.wordnet.synsets() for h in s.hypernyms()}
+            i = 0
+            while len(hypers) > 1 and i < self.highest_level:
+                hypers_pairs = combinations(hypers, 2)
+                hypers = {h for h1, h2 in hypers_pairs for h in h1.lowest_common_hypernyms(h2)}
+                i += 1
+
+            t._.set('hypernyms', hypers)
+            
+        return doc
+
+
+def tok_hypernyms_matcher(targets, highest_level=0, highest_common_level=0):
+    targets = {wn.synset(k) for k in targets}
+
+    def match(token):
+        matched = set()
+        
+        hypers_all = hypers_common = {h for s in token._.wordnet.synsets() for h in s.hypernyms()}
+        matched |= targets & hypers_all
+        for i in range(highest_level):
+            hypers_all = {h for s in hypers_all for h in s.hypernyms()}
+            if not hypers_all:
+                print("i=",i)
+                break
+            matched |= targets & hypers_all
+
+        for i in range(highest_common_level):
+            hypers_pairs = combinations(hypers_common, 2)
+            hypers_common = {h for h1, h2 in hypers_pairs for h in h1.lowest_common_hypernyms(h2)}
+            if not hypers_common:
+                print("j=",i)
+                break
+            matched |= targets & hypers_common
+
+        return matched
+    
+    return match
+
+
+class HypernymMatcher:
+    name = 'hypernym_match'
+    
+    def __init__(self, vocab, synset2tag, 
+                 pattern=None, highest_level=0, 
+                 attr_name='hypernym_match', 
+                 highest_common_level=0, force_ext=True):
+        Token.set_extension(attr_name, default=set(), force=force_ext)
+        self.synset2tag = {wn.synset(k):v for k, v in synset2tag.items()}
+        if pattern:
+            self.matcher = Matcher(vocab)
+            self.matcher.add(attr_name, None, pattern)
+        else:
+            self.matcher = None
+        
+        self.attr_name = attr_name
+        self.highest_level = highest_level
+        self.highest_common_level = highest_common_level
+    
+    def __call__(self, doc):
+        if self.matcher:
+            matches = self.matcher(doc)
+            print(f"[{self.__class__.__name__}] {len(matches)} matches")
+            toks = (doc[start:end].root for _, start, end in matches)
+        else:
+            toks= iter(doc)
+        
+        attr_name = self.attr_name
+        
+        mention_matcher = tok_hypernyms_matcher(self.synset2tag.keys(),
+                                                self.highest_level,
+                                                self.highest_common_level)
+
+        for t in toks:
+            matched = mention_matcher(t)
+            
+            if matched:
+                print(f"[{self.__class__.__name__}] matched for {t.text!r}: {matched} ")
+            tags = {self.synset2tag[x] for x in matched}
+            t._.set(attr_name, tags)
+            
+        return doc
+
+
+class EntityTagger:
+    name = 'entity_tag'
+
+    def __init__(self, collector, attr_name='ent_tags', force_ext=True):
+        """
+        collector: Function[Span] -> Set[Any]
+        """
+        Doc.set_extension(attr_name, default=None, force=force_ext)
+        Span.set_extension(attr_name, default=None, force=force_ext)
+        self.collector = collector
+        self.attr_name = attr_name
+    
+    def __call__(self, doc):
+        collector = self.collector
+        alltags = defaultdict(set)
+        for clust in doc._.coref_clusters:
+            clust_id = clust.i
+            collected = alltags[clust_id]
+            for mention in clust:
+                tags = collector(mention)
+                mention._.set(self.attr_name, tags)
+                collected.update(tags)
+        
+        doc._.set(self.attr_name, alltags)
+        return doc
