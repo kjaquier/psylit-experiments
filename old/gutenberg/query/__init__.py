86fca877ca02451a8d9643e55c83ffb8e19a0c40
Kevin Jaquier <kevin.jaquier@gmail.com>
2019-08-07 13:22:55 +1200

    Removed old code

--- old/gutenberg/query/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Package to deal with handling meta-data about Project Gutenberg works."""
-
-
-from gutenberg.query.api import get_metadata  # noqa
-from gutenberg.query.api import get_etexts  # noqa
-from gutenberg.query.api import list_supported_metadatas  # noqa
-
-from gutenberg.query.extractors import AuthorExtractor  # noqa
-from gutenberg.query.extractors import TitleExtractor  # noqa
