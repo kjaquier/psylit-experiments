87c06a2d40745c9bf31c3e97e6db3bc8ba84d118
Kevin Jaquier <kevin.jaquier@gmail.com>
2019-08-20 11:22:17 +1200

    Updated run scripts

--- run_prepare.sh
+++ run_prepare.sh
@@ -1,1 +1,3 @@
-for f in data/raw/$0 ; do python src/data_prepare.py -x $f data/interim/$0 ; done
+set -f
+set -x
+for f in data/raw/$1$(echo "/*.txt") ; do python src/data_prepare.py -x $f data/interim/$1 ; done
