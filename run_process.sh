1eeeb94322c9bf20e789d1a2e1ff6aefe51accb7
Kevin Jaquier <kevin.jaquier@gmail.com>
2019-08-28 10:49:58 +1200

    run_process: optionally skip if already ran

--- run_process.sh
+++ run_process.sh
@@ -1,3 +1,3 @@
 set -f
 set -x
-for f in data/interim/$1 ; do python src/data_process.py -x $f data/processed/$1 ; done
+for f in data/interim/$1 ; do python src/data_process.py -kx $f data/processed/$1 ; done
