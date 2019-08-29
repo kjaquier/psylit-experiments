3c7bd5419e0926b9417093344bfffac2db04d507
Kevin Jaquier <kevin.jaquier@gmail.com>
2019-08-30 11:30:09 +1200

    Various stuff

--- src/features/subsentences.py
+++ src/features/subsentences.py
@@ -18,3 +18,14 @@ def time_remapping(doc):
     idx = pd.IntervalIndex.from_breaks(breaks)
     n_intervals = len(idx)
     return pd.Series(np.arange(n_intervals), index=idx)
+
+
+def find_subsent_root(token):
+    if token.dep_ in DEP_INSIDE_SUBSENTENCE:
+        for ances in token.ancestors:
+    if ances._.get(predicate_flag):
+    ances._.patients.add(patient)
+    return ances
+    break
+
+    if ances._.agents or ances.dep_ not in DEP_INSIDE_SUBSENTENCE: # resolved or subsentence root
