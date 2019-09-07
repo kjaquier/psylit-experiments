19afd62b6587384e14e73fe904fe950c553c0c88
Kevin Jaquier <kevin.jaquier@gmail.com>
2019-09-07 12:00:35 +1200

    TE: skip if already done

--- run_appte.sh
+++ run_appte.sh
@@ -1,3 +1,3 @@
 set -x
 set -f
-python src/run_experiment.py TransferEntropy_StimulusResponse data/processed/$1/EN_* data/analytics
+python src/run_experiment.py -k TransferEntropy_StimulusResponse data/processed/$1/EN_* data/analytics
