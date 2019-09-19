0c96404b2da8e3eef4b49c172e5b1f22afde50c3
Kevin Jaquier <kevin.jaquier@gmail.com>
2019-09-19 22:40:20 +1200

    Added missing TE results

--- run_appte.sh
+++ run_appte.sh
@@ -1,3 +1,3 @@
 set -x
 set -f
-python src/run_experiment.py -ek TransferEntropy_StimulusResponse data/analytics/stimres/* data/experiments
+python src/run_experiment.py -ek TransferEntropy_StimulusResponse data/analytics/stimres_batch$1/* data/experiments
